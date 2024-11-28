5523944d6ee5a44ad273c88f5755389dbd9a5d3b
biswass101 <biswassnaeemcse@gmail.com>
2024-11-29 01:20:03 +0600

    cors setted up

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express")
+const cors = require('cors')
 const userRouter = require("./routes/users.route")
 const productRouter = require('./routes/poducts.route')
 const app = express();
 const port = 3000
 
+
+app.use(cors())
 app.use(express.urlencoded({extended : true}))
+// app.use(express.json())
+
 app.use(userRouter)
 app.use(productRouter)
 
